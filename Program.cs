9ae46f5de96d9b3b7f130bc1dd727f253bb50c91
Rafael <faelmota7@gmail.com>
2022-09-09 22:31:00 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("\n\t\t\tVelocidade da Luz");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine("Eu já não sei mais");
+Console.WriteLine("Por quê vivo a sofrer");
+Console.WriteLine("Pois eu nada fiz");
+Console.WriteLine("Para merecer");
+Console.WriteLine();
+Console.WriteLine("Te dei carinho amor");
+Console.WriteLine("Em troca ganhei ingratidão");
+Console.WriteLine("Não sei porquê, mas acho");
+Console.WriteLine("Que é falta de compreensão");
+Console.WriteLine();
+Console.WriteLine("Você me tem como réu");
+Console.WriteLine("O culpado e o ladrão");
+Console.WriteLine("Por tentar ganhar seu coração");
+Console.WriteLine();
+Console.WriteLine("Te dei carinho amor");
+Console.WriteLine("Em troca ganhei ingratidão");
+Console.WriteLine("Não sei porquê, mas acho");
+Console.WriteLine("Que é falta de compreensão");
+Console.WriteLine();
+Console.WriteLine("Você me tem como réu");
+Console.WriteLine("O culpado e o ladrão");
+Console.WriteLine("Por tentar ganhar seu coração");
+Console.WriteLine();
+Console.WriteLine("Todo mundo erra");
+Console.WriteLine("Todo mundo erra sempre");
+Console.WriteLine("Todo mundo vai errar");
+Console.WriteLine("Não sei porquê meu Deus");
+Console.WriteLine("Sozinho eu vivo a penar");
+Console.WriteLine();
+Console.WriteLine("Não tenho nada a pedir");
+Console.WriteLine("Também não tenho nada a dar");
+Console.WriteLine("Por isso é que eu vou me mandar");
+Console.WriteLine("Vou me embora agora");
+Console.WriteLine();
+Console.WriteLine("Vou me embora agora");
+Console.WriteLine("Vou embora prá outro planeta");
+Console.WriteLine("Na velocidade da luz");
+Console.WriteLine("Ou quem sabe de um cometa");
+Console.WriteLine();
+
+
+
